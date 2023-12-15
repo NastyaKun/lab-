2643cd93ed00f57cd9e93e2a3d1efd700f6e94e8
NastyaKun <148327680+NastyaKun@users.noreply.github.com>
2023-12-15 10:27:31 +0200

    Create 3.cpp

---
+++ lab11/3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main()
+{
+	int n, m, pos;
+	int maxsum = INT_MIN;
+	std::cin >> n >> m;
+	int** matrix = new int*[n];
+	for (int i = 0; i < n; i++) 
+		matrix[i] = new int[m];
+
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			std::cin >> matrix[i][j];
+
+	for (int i = 0; i < n; i++)
+	{
+		int sum = 0;
+		int el;
+		for (int j = 0; j < n; j++)
+		{
+			el = matrix[i][j];
+			sum += el;
+		}
+		if (sum > maxsum)
+		{
+			maxsum = sum;
+			pos = i;
+		}
+	}
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (i == pos) matrix[i][j] += 1;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			std::cout << matrix[i][j] << " ";
+		std::cout << std::endl;
+	}
+
+	for (int i = 0; i < n; i++) 
+		delete[] matrix[i];
+
+	delete[] matrix;
+}
